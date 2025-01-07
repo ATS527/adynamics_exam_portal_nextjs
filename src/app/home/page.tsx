e64c9304a8fb9167cd97b1554b4b29865c818c56
Binnukrishna <binnukrishna5233@gmail.com>
2025-01-07 15:59:24 +0530

    why choose us mobile updated

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,34 @@
 import LandinNav from "@/components/LandinNav";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ChartLine, Clock, Infinity as Unlimited, Trophy } from "lucide-react";
+import whyChooseUs from "@/constants/whyChooseUsData";
 
 const Home = () => {
+  const iconMap = {
+    clock: Clock,
+    "line-chart": ChartLine,
+    trophy: Trophy,
+    infinity: Unlimited,
+  };
+
   return (
     <>
       <LandinNav />
-      <header className="w-full h-screen">
+      <header className="w-full">
         {/* mobile hero */}
         <div className="w-full h-screen flex items-center justify-center relative sm:hidden">
           <div className="z-10 px-3 flex flex-col items-center justify-center">
             <h1 className="text-white text-6xl font-extrabold tracking-tight text-center">
-              Master Your <br></br> Aviation Journey
+              Crack Your <br></br> Aviation Journey
             </h1>
             <p className="text-white text-xs font-normal text-center mt-3">
               Unleash your full potential and soar through your aviation
@@ -39,9 +56,9 @@ const Home = () => {
           <div className="w-full h-full flex items-center justify-center relative">
             <div className="z-10 px-3 flex flex-col items-center justify-center w-full">
               <h1 className="text-white text-7xl font-extrabold tracking-tight text-center">
-                Master Your <br></br> Aviation Journey
+                Crack Your <br></br> Aviation Journey
               </h1>
-              <p className="text-white sm:w-[60%] lg:w-[40%] text-sm font-normal text-center mt-3">
+              <p className="text-white sm:w-[60%] lg:w-[40%] xl:w-[500px] text-sm font-normal text-center mt-3">
                 Unleash your full potential and soar through your aviation
                 challenges with a platform designed for success. From practice
                 tests to expert insights, we’re here to fuel your journey to the
@@ -64,7 +81,48 @@ const Home = () => {
         </div>
       </header>
 
-      <main className="w-full h-[200vh]"></main>
+      <main className="w-full flex flex-col items-center justify-center">
+        <section className="w-full lg:max-w-7xl">
+          <div className="flex items-start justify-start w-full flex-col px-4 mt-12 md:mt-20">
+            <h1 className="font-bold text-black text-3xl md:text-4xl">
+              Why Choose us?
+            </h1>
+            <p className="text-sm md:text-base mt-3 md:mt-4">
+              We are the most advanced and comprehensive test platform for DGCA,
+              CPL and ATPL exams.
+              <br />
+              trusted by more than thousands of students and professionals
+              around the world.
+            </p>
+          </div>
+
+          <div className="px-4 grid grid-cols-2 gap-3 mt-8">
+            {whyChooseUs.map((item) => {
+              const Icon = iconMap[item.icon];
+              return (
+                <Card key={item.title} className="w-full sm:w-3/12">
+                  <CardHeader>
+                    <Icon className="mb-2" />
+                    <CardTitle>{item.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <CardDescription>{item.description}</CardDescription>
+                  </CardContent>
+                </Card>
+              );
+            })}
+            {/* <Card className="w-1/2 sm:w-3/12">
+              <CardHeader>
+                <Clock className="mb-2"/>
+                <CardTitle>Real time exam tracking</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription>Know how long you have been practicing and see how much time you have left.</CardDescription>
+              </CardContent>
+            </Card> */}
+          </div>
+        </section>
+      </main>
     </>
   );
 };
